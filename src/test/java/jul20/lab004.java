fd10be15592615c51819cafef78a475f0294c1fb
Shaik Mahammad Basha <mohammadbasha7893@gmail.com>
2026-07-20 22:13:54 +0530

    code

---
+++ src/test/java/jul20/lab004.java
@@ -0,0 +1,14 @@
+package jul20;
+
+public class lab004 {
+    public static void main(String[] args) {
+        //swapping 2 number with 3rd varible
+        int a = 100;
+        int b = 200;
+        int c = a;
+        a = b;
+        b= c;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
